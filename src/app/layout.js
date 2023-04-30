2973c606ffe6c3ac619ffb64aab8d75539b40e09
tiagoinaba <tiago.inaba@gmail.com>
2023-04-30 13:41:51 -0300

    added dark mode

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import Header from '@/components/Header'
 import './globals.css'
 import { Inter } from 'next/font/google'
+import Providers from './Providers'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,15 +14,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body>
-        {/* Header */}
-        <Header />
+        <Providers>
+          {/* Header */}
+          <Header />
 
-        {/* Navbar */}
+          {/* Navbar */}
 
-        {/* SearchBox */}
+          {/* SearchBox */}
 
-        {children}
-        
+          {children}
+        </Providers>
       </body>
     </html>
   )
