90f3fe541f7f8587bc9d05aa9327cb3866e5040e
tiagoinaba <tiago.inaba@gmail.com>
2023-05-03 16:38:58 -0300

    add search ability

--- src/components/Card.js
+++ src/components/Card.js
@@ -8,19 +8,19 @@ export default function Card({result}) {
     <div className='cursor-pointer sm:p-3 sm:hover:shadow-slate-400 sm:shadow-md rounded-lg sm:border sm:border-slate-400 sm:m-2 transition-shadow duration-200 group'>
         <Link href={`/movie/${result.id}`}>
             <Image 
-            src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
-            width={500}
-            height={300}
-            className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
-            style={
-                {
-                    maxWidth: "100%",
-                    height: `auto`,
+                src={`https://image.tmdb.org/t/p/original/${result.backdrop_path || result.poster_path}`}
+                width={500}
+                height={300}
+                className='sm:rounded-t-lg group-hover:opacity-80 transition-opacity duration-200'
+                style={
+                    {
+                        maxWidth: "100%",
+                        height: `auto`,
+                    }
                 }
-            }
-            placeholder='blur'
-            blurDataURL='/spinner.svg'
-            alt='image is not available'
+                placeholder='blur'
+                blurDataURL='/spinner.svg'
+                alt='image is not available'
             ></Image>
             <div className="p-2">
                 <p className='line-clamp-2'>{result.overview}</p>
---
+++ src/components/SearchBox.js
@@ -0,0 +1,35 @@
+"use client"
+
+import { useRouter } from 'next/navigation'
+import React, {useState} from 'react'
+
+export default function SearchBox() {
+    const [search, setSearch] = useState('')
+    const router = useRouter()
+
+    function handleSubmit(e) {
+        e.preventDefault()
+        if(!search) return
+        router.push(`/search/${search}`)
+    }
+
+  return (
+    <form 
+        onSubmit={handleSubmit} 
+        className='flex max-w-6xl mx-auto justify-between items-center px-5'
+    >
+        <input 
+            type="text" 
+            placeholder='Search keywords...'
+            className='w-full h-14 rounded-sm placeholder-gray-500 outline-none bg-transparent flex-1'
+            value={search}
+            onChange={(e) => setSearch(e.target.value)}
+        />
+        <button 
+            type="submit" 
+            disabled={!search}
+            className='text-amber-600 disabled:text-gray-400'
+        >Search</button>
+    </form>
+  )
+}
